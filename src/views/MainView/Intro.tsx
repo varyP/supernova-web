e44913645147c18c34e916c739d7ce9cfc13a8e7
Varun Parakh <varun.parakh07@gmail.com>
2022-04-19 23:57:04 +0800

    Adjust height & spacing for home intro.

--- src/views/MainView/Intro.tsx
+++ src/views/MainView/Intro.tsx
@@ -3,16 +3,16 @@ import TopLinks from './../../layouts/TopBar/TopLink';
 export const Intro = () => {
 	return (
 		<section className="w-full bg-yellow-300 h-[36rem]" id="cff">
-			<div className="max-w-7xl mx-auto w-full mt-24 py-8 px-8">
+			<div className="max-w-7xl mx-auto w-full mt-24 py-2 px-8">
 				<h1 className="text-2xl sm:text-[6rem] text-black font-bold">Cool Furry Frens</h1>
 			</div>
-			<div className="max-w-7xl mx-auto w-full pb-16 px-8 space-y-8">
+			<div className="max-w-7xl mx-auto w-full pb-16 px-8 py-16 space-y-8">
 				<ol className="space-y-2 text-lg">
 					<li>
 						<span className="mr-2 text-white" role="img" aria-label="dna">
 						🪐
 						</span>{' '}
-						<span className="text-blue font-semibold">
+						<span className="text-blue font-medium">
 						<span className="text-blue-400 font-extrabold"> 6969 </span> cool 👽 met some 🐱 frens in the metaversal galaxy
 						</span>
 					</li>
@@ -20,7 +20,7 @@ export const Intro = () => {
 						<span className="mr-2" role="img" aria-label="water-wave">
 						🎫
 						</span>{' '}
-						<span className="text-blue">
+						<span className="text-blue font-medium">
 							hodl or trade your cool frens & be prepared for something special
 						</span>
 					</li>
@@ -28,7 +28,7 @@ export const Intro = () => {
 						<span className="mr-2" role="img" aria-label="eyes">
 							🤘
 						</span>{' '}
-						<span className="text-blue">
+						<span className="text-blue font-medium">
 							mint more, pay less! -- super low gas per mint!
 						</span>
 					</li>
